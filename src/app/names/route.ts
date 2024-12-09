ad84e1c0312953cc8c21cf57e0a63666da0a153b
ArthurCunha-Dev <arthurcunha1114@gmail.com>
2024-12-09 20:03:25 -0300

    add um query nos names

--- src/app/names/route.ts
+++ src/app/names/route.ts
@@ -1,13 +1,19 @@
+import { NextRequest } from 'next/server'
 import { data } from './data'
 
-export async function GET() {
-    return Response.json(data)
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('query')
+
+    const filteredNames = query ? data.filter(name => name.name.includes(query)) : data
+
+    return Response.json(filteredNames)
 }
 
 export async function POST(request: Request) {
     const name = await request.json()
     const newName = {
-        id: data.length = 1,
+        id: data.length + 1,
         name: name.name
     }
 
